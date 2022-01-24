4d4b4392fcb56cad6c08f10e19d004241a12b04e
Lukylix <dev.garcia.lucas@gmail.com>
2022-01-24 15:42:01 +0100

    Datalists

---
+++ scripts/datalist.js
@@ -0,0 +1,81 @@
+class dataList {
+	constructor(containerSelector, data, selectedCallBack = () => {}) {
+		this.data = data;
+		this.selectedCallBack = selectedCallBack;
+		this.parentContainer = document.querySelector(containerSelector).parentNode;
+		this.container = document.querySelector(containerSelector);
+		this.input = document.querySelector(containerSelector + " input[list]");
+		this.dataList = document.querySelector(containerSelector + " + datalist");
+		this.options = this.dataList.querySelectorAll("option");
+		this.selected = [];
+		this._setDataList();
+		this._addEventListeners();
+		this._setParentWidth();
+		console.log(this.parentContainer);
+	}
+	set dataOptions(data) {
+		this.data = data;
+		this._setDataList();
+		this._setParentWidth();
+	}
+
+	removeSelected(value) {
+		this.selected = this.selected.filter((item) => item !== value);
+		this.selectedCallBack();
+	}
+
+	_setDataList() {
+		this.dataList.innerHTML = "";
+		this.data.forEach((item) => {
+			let option = document.createElement("option");
+			option.value = item;
+			option.textContent = item;
+			this.dataList.appendChild(option);
+		});
+		this.options = this.dataList.querySelectorAll("option");
+	}
+	_setParentWidth() {
+		this.parentContainer.classList.remove("w30");
+		this.parentContainer.classList.remove("w20");
+		if (this.data.length > 20) {
+			this.parentContainer.classList.add("w30");
+			return;
+		}
+		if (this.data.length > 10) {
+			this.parentContainer.classList.add("w20");
+			return;
+		}
+	}
+	_addEventListeners() {
+		this.input.addEventListener("focus", () => {
+			this.dataList.style.display = "grid";
+			this.parentContainer.classList.add("active");
+		});
+		this.input.addEventListener("blur", () => {
+			setTimeout(() => {
+				this.dataList.style.display = "none";
+				this.parentContainer.classList.remove("active");
+			}, 100);
+		});
+		this.input.addEventListener("input", () => {
+			this.options.forEach((option) => {
+				if (option.value.toLowerCase().includes(this.input.value.toLowerCase())) {
+					option.style.display = "block";
+				} else {
+					option.style.display = "none";
+				}
+			});
+		});
+		this.options.forEach((option) => {
+			option.addEventListener("click", () => {
+				//this.input.value = option.value;
+				// Unique selection value
+				this.selected = [...new Set([...this.selected, option.value])];
+				this.selectedCallBack();
+				console.log(this.selected);
+			});
+		});
+	}
+}
+
+//new dataList("#ingrédient-container", ["Pomme", "Poire", "Cerise", "Pattate"]);
--- scripts/index.js
+++ scripts/index.js
@@ -27,17 +27,84 @@ function search(searchTerm) {
 	return results;
 }
 
+function getIngredients(recipes) {
+	const ingredients = [];
+	recipes.forEach((recipe) => {
+		recipe.ingredients.forEach((ingredient) => {
+			ingredients.push(ingredient.ingredient);
+		});
+	});
+	return [...new Set(ingredients)];
+}
+
+function getAppareils(recipes) {
+	const appareils = [];
+	recipes.forEach((recipe) => {
+		appareils.push(recipe.appliance);
+	});
+	return [...new Set(appareils)];
+}
+
+function getUstensiles(recipes) {
+	const ustensiles = [];
+	recipes.forEach((recipe) => {
+		recipe.ustensils.forEach((ustensile) => {
+			ustensiles.push(ustensile);
+		});
+	});
+	return [...new Set(ustensiles)];
+}
+
+function displayTags() {
+	const tagsContainer = document.querySelector(".tags");
+	tagsContainer.innerHTML = "";
+	const tags = [
+		{ type: "ingredients", selected: dataListIngredients.selected },
+		{ type: "appareils", selected: dataListAppareils.selected },
+		{ type: "ustensiles", selected: dataListUstensiles.selected },
+	];
+	tags.forEach((tag) => {
+		const type = tag.type;
+		tag.selected.forEach((selected) => {
+			const tagDom = document.createElement("span");
+			tagDom.classList.add("tag");
+			tagDom.classList.add(type);
+			tagDom.innerHTML = selected;
+			const closeTagDom = document.createElement("i");
+			closeTagDom.className = "far fa-times-circle";
+			closeTagDom.addEventListener("click", () => {
+				if (type === "ingredients") dataListIngredients.removeSelected(selected);
+				if (type === "appareils") dataListAppareils.removeSelected(selected);
+				if (type === "ustensiles") dataListUstensiles.removeSelected(selected);
+			});
+			tagDom.appendChild(closeTagDom);
+			tagsContainer.appendChild(tagDom);
+		});
+	});
+}
+
+const dataListIngredients = new dataList("#ingrédient-container", getIngredients(recipes), displayTags);
+const dataListAppareils = new dataList("#appareil-container", getAppareils(recipes), displayTags);
+const dataListUstensiles = new dataList("#ustensile-container", getUstensiles(recipes), displayTags);
+
 async function init() {
+	displayCards(recipes);
+
 	document.querySelector(".research-bar input").addEventListener("keyup", (event) => {
 		const input = event.target;
 		if (input.value.length > 2) {
 			const results = search(input.value);
 			displayCards(results);
+			dataListIngredients.dataOptions = getIngredients(results);
+			dataListAppareils.dataOptions = getAppareils(results);
+			dataListUstensiles.dataOptions = getUstensiles(results);
 		} else {
 			displayCards(recipes);
+			dataListIngredients.dataOptions = getIngredients(recipes);
+			dataListAppareils.dataOptions = getAppareils(recipes);
+			dataListUstensiles.dataOptions = getUstensiles(recipes);
 		}
 	});
-	displayCards(recipes);
 }
 
 init();
