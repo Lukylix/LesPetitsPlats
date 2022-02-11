ecbe8927b087d3b25d6fa4d1117f3b0b038e669f
Lukylix <dev.garcia.lucas@gmail.com>
2022-02-11 17:50:14 +0100

    UI fixes

--- scripts/factories/card.js
+++ scripts/factories/card.js
@@ -2,51 +2,53 @@ function cardFactory(recipe) {
 	const { name, ingredients, time, description } = recipe;
 
 	function getCardDom() {
-		const card = document.createElement("figure");
-		card.className = "card";
+		const figure = document.createElement("figure");
+		figure.className = "card";
 		const fakeImg = document.createElement("img");
 		fakeImg.className = "fake-image";
 		const figcaption = document.createElement("figcaption");
+		const recipeTitle = document.createElement("h2");
 
-		// const titleContainer = document.createElement("div");
-		// titleContainer.className = "title-container";
+		const recipeNameSpan = document.createElement("span");
+		recipeNameSpan.textContent = name;
 
-		const recipeName = document.createElement("h2");
-		recipeName.textContent = name;
 		const timeSpan = document.createElement("span");
+		timeSpan.className = "bold";
 		const clockIcon = document.createElement("i");
 		clockIcon.className = "far fa-clock";
-		const timeText = document.createElement("text");
-		timeText.textContent = ` ${time} min`;
+		const timeText = document.createTextNode(` ${time} min`);
 		timeSpan.appendChild(clockIcon);
 		timeSpan.appendChild(timeText);
 
-		recipeName.appendChild(timeSpan);
-		// titleContainer.appendChild(recipeName);
-		// titleContainer.appendChild(timeP);
+		recipeTitle.appendChild(recipeNameSpan);
+		recipeTitle.appendChild(timeSpan);
 
-		// const descriptionContainer = document.createElement("div");
-		// descriptionContainer.className = "description-container";
 		const ingredientList = document.createElement("ul");
 		ingredients.forEach((ingredient) => {
 			const { ingredient: name, quantity, unit } = ingredient;
 			const listItem = document.createElement("li");
-			listItem.textContent = `${name} ${quantity ? quantity : ""} ${unit ? unit : ""}`;
+
+			const ingredientNameSpan = document.createElement("span");
+			ingredientNameSpan.textContent = `${name}${quantity ? ": " : ""}`;
+			ingredientNameSpan.className = "bold";
+
+			const ingredientQuantity = document.createTextNode(`${quantity || ""} ${unit || ""}`);
+
+			listItem.appendChild(ingredientNameSpan);
+			listItem.appendChild(ingredientQuantity);
 			ingredientList.appendChild(listItem);
 		});
+
 		const descriptionP = document.createElement("p");
 		descriptionP.textContent = description;
-		// descriptionContainer.appendChild(ingredientList);
-		// descriptionContainer.appendChild(descriptionP);
 
-		figcaption.appendChild(recipeName);
+		figcaption.appendChild(recipeTitle);
 		figcaption.appendChild(ingredientList);
 		figcaption.appendChild(descriptionP);
-		// figcaption.appendChild(titleContainer);
-		// figcaption.appendChild(descriptionContainer);
-		card.appendChild(fakeImg);
-		card.appendChild(figcaption);
-		return card;
+
+		figure.appendChild(fakeImg);
+		figure.appendChild(figcaption);
+		return figure;
 	}
 	return { getCardDom };
 }
